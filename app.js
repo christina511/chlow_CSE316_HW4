3831487f9c7e09625e884132811cc5b48cd0cebd
Christina L <christina511ny@gmail.com>
2020-11-13 13:48:31 -0500

    fixed order by day function

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var sql = require("mysql");
+var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "pass4root",
-    database: "mydb"
+    password: "25XOvyaKRu",
+    database: "cse316hw4"
 });
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const url = require('url');
 
@@ -39,6 +39,13 @@ function writeSearch(req, res) {
     <title> Spring 2021 CSE Class Find </title>
 </head>
 
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+<style type = text/css>
+body{
+    font-family: Quicksand;
+    }
+</style>
+
 <body>
     <h1> Spring 2021 CSE Class Find </h1><br>
     <form method = "get" action ="/">
@@ -46,7 +53,7 @@ function writeSearch(req, res) {
         <b>in</b>
         <select name="filter">
             <option value= "allFields">All Fields</option>
-            <option value= "courseName">Course Title</option>
+            <option value= "Title">Course Title</option>
             <option value= "courseNum">Course Num</option>
             <option value= "instructor">Instructor</option>
             <option value= "day">Day</option>
@@ -66,7 +73,7 @@ if (filter == "allFields")
     sql = `SELECT * FROM courses
             WHERE Subject       LIKE '%` + search + `%' OR
                 Course          LIKE '%` + search + `%' OR
-                CourseName      LIKE '%` + search + `%' OR
+                Title           LIKE '%` + search + `%' OR
                 Component       LIKE '%` + search + `%' OR
                 Section         LIKE '%` + search + `%' OR
                 Days            LIKE '%` + search + `%' OR
@@ -88,9 +95,9 @@ if (filter == "allFields")
         sql = `SELECT * FROM courses
             WHERE Course        LIKE '%` + search + `%';`;
         //sql to search course names
-        else if (filter == "courseName")
+        else if (filter == "Title")
             sql = `SELECT * FROM courses
-            WHERE CourseName    LIKE '%` + search + `%';`;
+            WHERE Title    LIKE '%` + search + `%';`;
     //sql to search instructors
     else if (filter == "instructor")
         sql = `SELECT * FROM courses
@@ -99,7 +106,7 @@ if (filter == "allFields")
     else if (filter == "day")
         sql = `SELECT * FROM courses
             WHERE Days        LIKE '%` + search + `%'
-            ORDER BY StartTimeInternal;`;
+            ORDER BY STR_TO_DATE(StartTime, '%h:%i %p');`;
     //sql to search course times
     else if (filter == "time")
         sql = `SELECT * FROM courses
@@ -110,8 +117,8 @@ if (filter == "allFields")
         if(err) throw err;
         for(let item of result){
             html += `
-            <button type="button" class="toggle> CSE ` + item.Course + ` - ` +
-            item.CourseName + ` - ` + item.Component + ` - Section ` + item.Section + `</button>
+            <button type="button" class="toggle"> CSE ` + item.Course + ` - ` +
+            item.Title + ` - ` + item.Component + ` - Section ` + item.Section + `</button>
             <pre>
                 Days: ` + item.Days + `
                 Start Time: ` + item.StartTime + `
@@ -135,3 +142,110 @@ if (filter == "allFields")
 
 };
 
+function writeSchedule(req,res) {
+    let query = url.parse(req.url, true).query;
+    let addQuery = `INSERT INTO saved SELECT * FROM courses WHERE courses.id="`+ query.add + `";`
+
+    let html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title> Schedule </title>
+            <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+            <style type = text/css>
+                table, tr, th, td {
+                    border: 1px solid black;
+                    height: 50px;
+                    vertical-align: bottom;
+                    padding: 15px;
+                    text-align: center;
+                    border-spacing: 0px;
+                    line-height: 15px;
+                }
+                body{
+                    font-family: Quicksand;
+                }
+                th {
+                  border: 1px solid #ffffff;
+                  background-color: lavender;
+                  text-align: center;
+                  padding: 8px;
+                  border-collapse: collapse;
+                }
+            </style>
+        </head>
+        <body>
+                <h1> Schedule </h1><br>
+                <a href="/"><b>Return to Search</b></a>
+                <br><br>
+
+                <table>
+                    <tr>
+                        <th> Mon </th>
+                        <th> Tue </th>
+                        <th> Wed </th>
+                        <th> Thu </th>
+                        <th> Fri </th>
+                    </tr>
+                    <tr>
+                        <td> Mon </td>
+                        <td> Tue </td>
+                        <td> Wed </td>
+                        <td> Thu </td>
+                        <td> Fri </td>
+                    </tr>
+                </table>
+        </body>
+        </html>
+    `;
+
+    con.query(addQuery, function(err, result) {
+        if (err) console.log(err);
+        con.query(constructSQLDayCommand("M"), function(err, result) {
+            if (err) throw err;
+            html = html.replace("<td> Mon </td>", getDay(result, "MON"));
+            con.query(constructSQLDayCommand("TU"), function(err, result) {
+                if (err) throw err;
+                html = html.replace("<td> Tue </td>", getDay(result, "TUE"));
+                con.query(constructSQLDayCommand("W"), function(err, result) {
+                    if (err) throw err;
+                    html = html.replace("<td> Wed </td>", getDay(result, "WED"));
+                    con.query(constructSQLDayCommand("TH"), function(err, result) {
+                        if (err) throw err;
+                        html = html.replace("<td> Thu </td>", getDay(result, "THU"));
+                        con.query(constructSQLDayCommand("F"), function(err, result) {
+                            if (err) throw err;
+                            html = html.replace("<td> Fri </td>", getDay(result, "FRI"));
+                            res.write(html + "\n\n</body>\n</html>");
+                            res.end();
+                    });
+                });    
+            });
+        });
+    });
+
+
+    });
+}
+
+function getDay(SQLResult, tableHeader) {
+    let retStr = "<td>";
+    for (let item of SQLResult) {
+        retStr += "\n       <b> " + item.StartTime + " - " +
+                                    item.EndTime + " <br><br>" +
+                                    item.Subject + " " +
+                                    item.Course + "-" +
+                                    item.Section + " </b> <p> " +
+                                    item.Title + "<br><br>" +
+                                    item.Instructor + "<br><br>" +
+                                    "<br/><br/>";
+    }
+    return retStr + "</td>";
+}
+
+function constructSQLDayCommand(search) {
+    var sql = `SELECT * FROM saved
+                WHERE Days              LIKE '%` + search + `%'
+                ORDER BY STR_TO_DATE(StartTime, '%h:%i %p');`;
+    return sql;
+};
